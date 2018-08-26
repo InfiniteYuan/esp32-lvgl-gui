78ec26cd63c5e2e3b5166cd062002dedc3e15ab2
InfiniteYuan <infiniteyuan@outlook.com>
2018-08-26 22:36:45 +0800

    update submodule

---
+++ components/general/param/include/iot_param.h
@@ -0,0 +1,62 @@
+// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef _IOT_PARAM_H_
+#define _IOT_PARAM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+  * @brief  save param to flash with protect, based on NVS flash APIs
+  *
+  * @param  namespace
+  * @param  key different param should have different key
+  * @param  param
+  * @param  len
+  *
+  * @return
+  *     - ESP_OK: succeed
+  *     - others: fail
+  */
+esp_err_t iot_param_save(const char* namespace, const char* key, void* param, uint16_t len);
+
+/**
+  * @brief  read param from flash, based on NVS flash APIs
+  *
+  * @param  namespace
+  * @param  key different param should have different key
+  * @param  dest the address to save read param
+  *
+  * @return
+  *     - ESP_OK: succeed
+  *     - others: fail
+  */
+esp_err_t iot_param_load(const char* namespace, const char* key, void* dest);
+
+/**
+  * @brief  Erase param from flash, based on NVS flash APIs
+  *
+  * @param  namespace
+  * @param  key different param should have different key
+  *
+  * @return
+  *     - ESP_OK: succeed
+  *     - others: fail
+  */
+esp_err_t iot_param_erase(const char* namespace, const char* key);
+
+#ifdef __cpp
+}
+#endif
+#endif
