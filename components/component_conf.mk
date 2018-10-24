f977eef2c3ee61e22bdd5cef47718d23d5b695fb
InfiniteYuan1 <infiniteyuan@outlook.com>
2018-10-24 17:26:02 +0800

    update lvgl submodule. add theme example

--- components/component_conf.mk
+++ components/component_conf.mk
@@ -6,7 +6,7 @@
 # LVGL_COMPONENT_PATH ?= $(abspath $(shell pwd)/../)
 
 LVGL_COMPONENT_DIRS += $(LVGL_COMPONENT_PATH)/components
-LVGL_COMPONENT_DIRS += $(LVGL_COMPONENT_DIRS)/components/general
+LVGL_COMPONENT_DIRS += $(LVGL_COMPONENT_PATH)/components/general
 LVGL_COMPONENT_DIRS += $(LVGL_COMPONENT_PATH)/components/spi_devices
 LVGL_COMPONENT_DIRS += $(LVGL_COMPONENT_PATH)/components/i2c_devices
 LVGL_COMPONENT_DIRS += $(LVGL_COMPONENT_PATH)/components/i2c_devices/sensor
